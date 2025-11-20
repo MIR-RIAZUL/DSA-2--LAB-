4054282a9b45c53513edc022a986cd29dd173776
MIR-RIAZUL <mirriazul859@gmail.com>
2025-11-20 20:50:04 +0600

    map pass by referenace

--- from slide/map.cpp
+++ from slide/map.cpp
@@ -2,6 +2,23 @@
  #include<map>
 
  using namespace std;
+
+void print(map<string,int>&r)
+{
+    for(auto mal:r)
+    {
+        cout<<mal.first<<"->"<<mal.second<<endl;
+    }
+}
+void makepair(map<string,int>&r,string &name,int &age){
+    r.insert(make_pair(name,age));
+   
+
+    print(r);
+
+}
+
+
   int main()
   {
     map<string, int>fruit;
@@ -23,6 +40,24 @@
     for(auto i:wife_kids)
     {
         cout<<i.first<<"->"<<i.second.first<<","<<"baccha kaccha: "<<i.second.second<<endl;
+    }
+
+    //map pass by referance
+
+    map<string,int>r;
+    cout<<"how many pair you want to add in this map:"<<endl;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        string name;
+        cout<<"enter name:"<<endl;
+        cin>>name;
+
+        int age;
+        cout<<"enter the age:"<<endl;
+        cin>>age;
+        makepair(r,name,age);
     }
     return 0;
   }
