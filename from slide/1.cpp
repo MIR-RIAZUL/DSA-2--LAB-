1a1df94e9db2aff2d82499c024240651a43249c1
MIR-RIAZUL <mirriazul859@gmail.com>
2025-12-02 21:28:08 +0600

    slide code

---
+++ from slide/1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int even(int arr[], int low, int high)
+{
+    if (low == high)
+    {
+        if (arr[low] % 2 == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    int mid = (low + high) / 2;
+    int left = even(arr, low, mid);
+    int right = even(arr, mid + 1, high);
+    return left + right;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int m = even(arr, 0, n - 1);
+    cout << m << endl;
+}
