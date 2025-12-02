5917fdb068297546131c7003b032cd4d96ebc41d
MIR-RIAZUL <mirriazul859@gmail.com>
2025-12-02 18:08:52 +0600

    treaned version

--- my practice/maxsubarray_bydevideandconqure.cpp
+++ my practice/maxsubarray_bydevideandconqure.cpp
@@ -8,32 +8,19 @@ int maxCrosssum(int a[], int low, int mid, int high)
     int leftsum = INT_MIN;
     int sum = 0;
 
-    cout << "left sum" << endl
-         << "-----------------" << endl;
     for (int i = mid; i >= low; i--)
     {
         sum = sum + a[i];
-        cout << "sum" << i << ":" << sum << endl;
         leftsum = max(leftsum, sum);
-        cout << "Ls" << i << ":" << leftsum << endl; 
     }
-    cout << "----------------" << endl
-         << endl;
     int rightsum = INT_MIN;
     sum = 0;
-    cout << "light sum" << endl
-         << "------------" << endl;
+
     for (int j = mid + 1; j <= high; j++)
     {
         sum = sum + a[j];
-
-        cout << "sum" << j << ":" << sum << endl;
         rightsum = max(rightsum, sum);
-        cout << "Rs" << j << ":" << rightsum << endl;
     }
-    cout << "-------------" << endl
-         << endl;
-    cout << "final sum" << leftsum + rightsum << endl;
     return leftsum + rightsum;
 }
 
@@ -49,7 +36,7 @@ int maxsubarray(int a[], int low, int high)
     int rightsum = maxsubarray(a, mid + 1, high);
     int crosssum = maxCrosssum(a, low, mid, high);
 
-    return max(leftsum,max(rightsum,crosssum));
+    return max(leftsum, max(rightsum, crosssum));
 }
 
 int main()
@@ -64,12 +51,8 @@ int main()
     {
         cin >> a[i];
     }
-    // for(int i=0;i<n;i++)
-    // {
-    //     cout<<a[i]<<" ";
-    // }
 
     int maxsum = maxsubarray(a, 0, n - 1);
-    cout<<"Maximum sunarray sum="<<maxsum<<endl;
+    cout << "Maximum sunarray sum=" << maxsum << endl;
     return 0;
 }
