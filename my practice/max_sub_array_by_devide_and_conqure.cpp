52c4a98a11b4cdcb18fd158b607bd9d758a59fa4
MIR-RIAZUL <mirriazul859@gmail.com>
2025-12-02 21:07:25 +0600

    update

--- my practice/max_sub_array_by_devide_and_conqure.cpp
+++ my practice/max_sub_array_by_devide_and_conqure.cpp
@@ -2,82 +2,36 @@
 #include <climits>
 using namespace std;
 
-// maxCrosssum calculates max sum crossing mid and updates start/end indices
-int maxCrosssum(int a[], int low, int mid, int high, int &crossStart, int &crossEnd)
-{
-    int leftsum = INT_MIN;
-    int sum = 0;
-    int maxLeft = mid;
-
-    for (int i = mid; i >= low; i--)
-    {
+int maxCrosssum(int a[], int low, int mid, int high) {
+    int leftsum = INT_MIN, sum = 0;
+    for (int i = mid; i >= low; i--) {
         sum += a[i];
-        if (sum > leftsum)
-        {
-            leftsum = sum;
-            maxLeft = i;
-        }
+        if (sum > leftsum) leftsum = sum;
     }
 
     int rightsum = INT_MIN;
     sum = 0;
-    int maxRight = mid + 1;
-
-    for (int j = mid + 1; j <= high; j++)
-    {
+    for (int j = mid + 1; j <= high; j++) {
         sum += a[j];
-        if (sum > rightsum)
-        {
-            rightsum = sum;
-            maxRight = j;
-        }
+        if (sum > rightsum) rightsum = sum;
     }
 
-    crossStart = maxLeft;
-    crossEnd = maxRight;
     return leftsum + rightsum;
 }
 
-// maxsubarray calculates max sum and updates start/end indices
-int maxsubarray(int a[], int low, int high, int &maxStart, int &maxEnd)
-{
+int maxsubarray(int a[], int low, int high) {
     if (low == high)
-    {
-        maxStart = low;
-        maxEnd = high;
         return a[low];
-    }
 
     int mid = (low + high) / 2;
+    int leftsum = maxsubarray(a, low, mid);
+    int rightsum = maxsubarray(a, mid + 1, high);
+    int crosssum = maxCrosssum(a, low, mid, high);
 
-    int leftStart, leftEnd, rightStart, rightEnd, crossStart, crossEnd;
-
-    int leftsum = maxsubarray(a, low, mid, leftStart, leftEnd);
-    int rightsum = maxsubarray(a, mid + 1, high, rightStart, rightEnd);
-    int crosssum = maxCrosssum(a, low, mid, high, crossStart, crossEnd);
-
-    if (leftsum >= rightsum && leftsum >= crosssum)
-    {
-        maxStart = leftStart;
-        maxEnd = leftEnd;
-        return leftsum;
-    }
-    else if (rightsum >= leftsum && rightsum >= crosssum)
-    {
-        maxStart = rightStart;
-        maxEnd = rightEnd;
-        return rightsum;
-    }
-    else
-    {
-        maxStart = crossStart;
-        maxEnd = crossEnd;
-        return crosssum;
-    }
+    return max(leftsum, max(rightsum, crosssum));
 }
 
-int main()
-{
+int main() {
     int n;
     cout << "Enter the size of the array: ";
     cin >> n;
@@ -87,14 +41,8 @@ int main()
     for (int i = 0; i < n; i++)
         cin >> a[i];
 
-    int maxStart, maxEnd;
-    int maxsum = maxsubarray(a, 0, n - 1, maxStart, maxEnd);
-
+    int maxsum = maxsubarray(a, 0, n - 1);
     cout << "Maximum subarray sum = " << maxsum << endl;
-    cout << "Subarray elements: ";
-    for (int i = maxStart; i <= maxEnd; i++)
-        cout << a[i] << " ";
-    cout << endl;
 
     return 0;
 }
