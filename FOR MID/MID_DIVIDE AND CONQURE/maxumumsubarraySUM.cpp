518ffab123cb1137f4980bce3e3b6c95e0392a5a
MIR-RIAZUL <mirriazul859@gmail.com>
2025-12-25 23:49:10 +0600

    ..

---
+++ FOR MID/MID_DIVIDE AND CONQURE/maxumumsubarraySUM.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"enter the array size:";
+    int n;
+    cin>>n;
+    int arr[n];
+
+    cout<<"enter the array:";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    
+}
