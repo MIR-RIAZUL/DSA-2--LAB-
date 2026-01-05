9b414ac53bb85c80f3cc451b0fa3f15004afa970
MIR-RIAZUL <mirriazul859@gmail.com>
2026-01-05 21:03:34 +0600

    ...

--- FOR MID/MID_DIVIDE AND CONQURE/maxumumsubarraySUM.cpp
+++ FOR MID/MID_DIVIDE AND CONQURE/maxumumsubarraySUM.cpp
@@ -1,5 +1,48 @@
 #include<iostream>
+#include <algorithm>
+#include <climits>
+
 using namespace std;
+
+
+int maxcrossSum(int arr[],int l,int m,int r)
+{
+    int sum=0;
+    int leftsum=INT_MIN;
+    for( int i=m;i>=l;i--)
+    {
+        sum=sum+arr[i];
+        leftsum=max(leftsum,sum);
+    }
+
+       sum=0;
+    int rightsum=INT_MIN;
+    for( int i=m+1;i<=r;i++)
+    {
+        sum=sum+arr[i];
+        rightsum=max(rightsum,sum);
+    }
+    return leftsum+rightsum;
+}
+
+
+int maxsubarray(int arr[],int l,int r)
+{
+    if(l==r)
+    {
+        return arr[l];
+    }
+    int m=(l+r)/2;
+    return max(
+        {
+            maxsubarray(arr,l,m),
+            maxsubarray(arr,m+1,r),
+            maxcrossSum(arr,l,m,r)
+
+        }
+    );
+
+}
 
 int main()
 {
@@ -13,5 +56,8 @@ int main()
     {
         cin>>arr[i];
     }
-    
+    int v=maxsubarray(arr,0,n-1);
+
+    cout<<v<<endl;
+
 }
